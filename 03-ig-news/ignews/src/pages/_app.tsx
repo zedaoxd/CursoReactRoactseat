c964463eb9e5805aaba0727c62ec2809eeaabb75
Bruno Lessa <brunolessa@live.com>
2023-01-06 02:15:35 -0300

    scss

--- 03-ig-news/ignews/src/pages/_app.tsx
+++ 03-ig-news/ignews/src/pages/_app.tsx
@@ -1,5 +1,16 @@
+import { Roboto } from "@next/font/google";
 import type { AppProps } from "next/app";
+import "../styles/global.scss";
+
+const roboto = Roboto({
+  weight: ["400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
