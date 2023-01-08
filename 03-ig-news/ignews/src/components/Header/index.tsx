120788b00df9dc323acb1f348c2c37fc3897f400
Bruno Lessa <brunolessa@live.com>
2023-01-07 21:46:39 -0300

    header

---
+++ 03-ig-news/ignews/src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" width={100} height={100} />
+        <nav>
+          <a href="" className={styles.active}>
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- 03-ig-news/ignews/src/pages/_app.tsx
+++ 03-ig-news/ignews/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Roboto } from "@next/font/google";
 import type { AppProps } from "next/app";
+import Header from "../components/Header";
 import "../styles/global.scss";
 
 const roboto = Roboto({
@@ -10,6 +11,7 @@ const roboto = Roboto({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className}>
+      <Header />
       <Component {...pageProps} />
     </main>
   );
