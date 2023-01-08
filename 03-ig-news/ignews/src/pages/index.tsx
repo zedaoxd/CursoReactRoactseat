3278e12fddbf9a64c68cdc41ce11ec6d68bd9dcf
Bruno Lessa <brunolessa@live.com>
2023-01-07 22:20:06 -0300

    home scss

--- 03-ig-news/ignews/src/pages/index.tsx
+++ 03-ig-news/ignews/src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   );
 }
