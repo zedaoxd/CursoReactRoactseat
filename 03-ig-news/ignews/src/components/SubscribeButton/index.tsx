5e2843dc367ac487a833ddbe9f5e3b4b7d395c00
Bruno Lessa <brunolessa@live.com>
2023-01-07 22:27:26 -0300

    button subscribe

---
+++ 03-ig-news/ignews/src/components/SubscribeButton/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const SubscribeButton = () => {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+};
+
+export default SubscribeButton;
--- 03-ig-news/ignews/src/pages/index.tsx
+++ 03-ig-news/ignews/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+import SubscribeButton from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -19,6 +19,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
