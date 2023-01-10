a47d2f11d36ca5a28863666faefc50d1a8f63453
Bruno Lessa <brunolessa@live.com>
2023-01-10 13:31:05 -0300

    stripe payment

--- 03-ig-news/ignews/src/components/SubscribeButton/index.tsx
+++ 03-ig-news/ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -6,8 +8,32 @@ type SubscribeButtonProps = {
 };
 
 const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId: sessionId });
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
